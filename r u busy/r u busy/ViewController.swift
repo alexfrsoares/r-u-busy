f68e79d2b1c8b8080aba8e49dd8dca021114b26a
Alex <otimizalex@outlook.com>
2021-12-01 12:07:22 +0000

    project files created

---
+++ r u busy/r u busy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  r u busy
+//
+//  Created by Alex Freitas on 01/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
